0c93b3d7892bb65933ddceee9d1863f90c749dcb
wp <2838839039@qq.com>
2022-12-31 16:04:48 +0800

    后台导出（权限控制）

---
+++ framework/src/main/java/com/apple/domain/vo/ExcelCategoryVo.java
@@ -0,0 +1,22 @@
+package com.apple.domain.vo;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExcelCategoryVo {
+
+    @ExcelProperty("分类名")
+    private String name;
+
+    @ExcelProperty("描述")
+    private String description;
+
+    //状态0：正常，1禁用
+    @ExcelProperty("状态0：正常，1禁用")
+    private String status;
+}
--- framework/src/main/java/com/apple/service/impl/UserDetailsServiceImpl.java
+++ framework/src/main/java/com/apple/service/impl/UserDetailsServiceImpl.java
@@ -1,7 +1,9 @@
 package com.apple.service.impl;
 
+import com.apple.constants.SystemConstants;
 import com.apple.domain.entity.LoginUser;
 import com.apple.domain.entity.User;
+import com.apple.mapper.MenuMapper;
 import com.apple.mapper.UserMapper;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -19,6 +22,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private MenuMapper menuMapper;
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
@@ -31,7 +37,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
             throw new RuntimeException("用户不存在");
         }
         //返回用户信息
-        //TODO 查询权限信息封装
-        return new LoginUser(user);
+        //查询权限信息封装
+        //如果是后台用户才需要查询权限封装
+        if (user.getType().equals(SystemConstants.ADMIN)){
+            List<String> list = menuMapper.selectPermsByUserId(user.getId());
+            return new LoginUser(user,list);
+        }
+        return new LoginUser(user,null);
     }
 }
